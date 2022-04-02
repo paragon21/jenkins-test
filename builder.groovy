4dd2f71e4ec9c38c86689fc185eea580b7cdb901
Vdovin Konstantin <kvdovin@vtb.ru>
2022-04-02 14:51:18 +0300

    Add new conf

--- builder.groovy
+++ builder.groovy
@@ -3,7 +3,7 @@ import groovy.json.JsonSlurper
 def jsonSlurper = new JsonSlurper()
 def jsonText = readFileFromWorkspace('src.json')
 
-def config = jsonSlurper.parseText(releaseScript)
+def config = jsonSlurper.parseText(jsonText)
 
 
 println(props)
